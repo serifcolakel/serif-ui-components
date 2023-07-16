b688ffee863fbc23f4263a3b4fac5aa9681d0c12
serifcolakel <serif.colakel0@gmail.com>
2023-07-16 11:29:34 +0300

    FIXED (serif) : Storybook button component icon issue fixed with react-icons package added.

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,12 +1,42 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import {
+  FaBeer,
+  Fa500Px,
+  FaAccessibleIcon,
+  FaAccusoft,
+  FaAcquisitionsIncorporated,
+  FaAd,
+  FaAddressCard,
+} from 'react-icons/fa';
 import Button from './Button';
+
+const buttonIcons: Record<string, React.ReactNode> = {
+  faBeer: <FaBeer />,
+  fa500Px: <Fa500Px />,
+  faAccessibleIcon: <FaAccessibleIcon />,
+  faAccusoft: <FaAccusoft />,
+  faAcquisitionsIncorporated: <FaAcquisitionsIncorporated />,
+  faAd: <FaAd />,
+  faAddressCard: <FaAddressCard />,
+};
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
   title: 'Components/Button',
   component: Button,
   tags: ['autodocs'],
-  argTypes: {},
+  argTypes: {
+    leftIcon: {
+      description: 'The button left icon (optional)',
+      options: Object.keys(buttonIcons),
+      mapping: buttonIcons,
+    },
+    rightIcon: {
+      description: 'The button right icon (optional)',
+      options: Object.keys(buttonIcons),
+      mapping: buttonIcons,
+    },
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
